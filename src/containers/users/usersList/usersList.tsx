61de093ab1f50cb51907a7672be0e462f1f0a53e
Наталья Оскарова <gooseberry.bay@gmail.com>
2023-10-24 20:40:40 +0300

    186208744 - Страница пользователей - scroll

--- src/containers/users/usersList/usersList.tsx
+++ src/containers/users/usersList/usersList.tsx
@@ -1,15 +1,31 @@
 import styles from './usersList.module.scss'
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect, useRef, useState} from "react";
 import {TStore} from "../../../slices/store";
 import UserCard from "./userCard/userCard";
+import {nextPage, setLoading} from "../../../slices/user";
 
 const UserList: React.FC = () => {
-
+    const dispatch = useDispatch();
     const userState = useSelector((state: TStore) => state.user)
 
 
+
+    const onScroll = async (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
+        const {scrollTop, scrollHeight, clientHeight} = e.currentTarget;
+        const isNearBottom = scrollTop + clientHeight >= scrollHeight - 500;
+
+        if (isNearBottom && !userState.loading) {
+            if (userState.users.length < userState.count) {
+                dispatch(setLoading(true));
+                await dispatch(nextPage());
+            }
+        }
+    }
+
+
     return (
-        <div className={styles.cardsList}>
+        <div className={styles.cardsList} onScroll={onScroll}>
             <ul className={styles.cardsWrap}>
                 {userState.users.map(user => <UserCard user={user} key={user.id}/>)}
             </ul>
