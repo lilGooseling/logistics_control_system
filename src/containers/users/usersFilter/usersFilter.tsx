2a9d586803907d1e5ee05f9500038956fd901f9d
Наталья Оскарова <gooseberry.bay@gmail.com>
2023-10-25 21:04:41 +0300

    186208744 - Страница пользователей - search

--- src/containers/users/usersFilter/usersFilter.tsx
+++ src/containers/users/usersFilter/usersFilter.tsx
@@ -1,9 +1,31 @@
 import styles from './usersFilter.module.scss';
+import {useEffect, useState} from "react";
+import {IUser, IUserStore} from "../../../interfaces/IUser";
+import {useDispatch, useSelector} from "react-redux";
+import {setQuery} from "../../../slices/user";
+import {TStore} from "../../../slices/store";
 
 const UsersFilters: React.FC = () => {
+    const dispatch = useDispatch();
+    const userState = useSelector((state: TStore) => state.user)
+    const [search, setSearch] = useState("");
+
+    useEffect(()=>{
+        realSetSearch();
+    },[search]);
+
+    const realSetSearch = () => {
+        dispatch(setQuery(search));
+    }
+
 
     return (
-        <div className={styles.filter}/>
+        <div className={styles.filter}>
+            <h1 className={styles.header}>Поиск</h1>
+            <div className={styles.search}>
+                <input type="text" value={search} placeholder="поиск..." className={styles.searchBar} onChange={(e) => dispatch(setSearch(e.target.value))}/>
+            </div>
+        </div>
     );
 }
 
