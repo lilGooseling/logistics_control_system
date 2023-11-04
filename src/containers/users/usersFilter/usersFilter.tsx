f40d8f6eeae24648acabfe5bfd2c73a23d37485a
Наталья Оскарова <gooseberry.bay@gmail.com>
2023-11-04 23:03:22 +0300

    186208744 - Страница пользователей - filter and sort

--- src/containers/users/usersFilter/usersFilter.tsx
+++ src/containers/users/usersFilter/usersFilter.tsx
@@ -2,8 +2,9 @@ import styles from './usersFilter.module.scss';
 import {useEffect, useState} from "react";
 import {IUser, IUserStore} from "../../../interfaces/IUser";
 import {useDispatch, useSelector} from "react-redux";
-import {setQuery} from "../../../slices/user";
+import {setQuery, setUserSorting} from "../../../slices/user";
 import {TStore} from "../../../slices/store";
+import AdaptiveSortingComponent from "../../../components/AdaptiveSorting/adaptiveSortingComponent";
 
 const UsersFilters: React.FC = () => {
     const dispatch = useDispatch();
@@ -20,10 +21,21 @@ const UsersFilters: React.FC = () => {
 
 
     return (
-        <div className={styles.filter}>
-            <h1 className={styles.header}>Поиск</h1>
-            <div className={styles.search}>
-                <input type="text" value={search} placeholder="поиск..." className={styles.searchBar} onChange={(e) => dispatch(setSearch(e.target.value))}/>
+
+        <div className={styles.wrapper}>
+            <div className={styles.filter}>
+                <h1 className={styles.header}>Поиск</h1>
+                <div className={styles.search}>
+                    <input type="text" value={search} placeholder="поиск..." className={styles.searchBar} onChange={(e) => dispatch(setSearch(e.target.value))}/>
+                </div>
+            </div>
+            <div className={styles.sortWrap}>
+                <h2 className={styles.sort}>Сортировки:</h2>
+                <div className={styles.sortItem}>
+                    <AdaptiveSortingComponent keyName='id' name='Id' sorting={userState.adaptiveSorting['id']} changeHandle={(key)=>dispatch(setUserSorting(key))}/>
+                    <AdaptiveSortingComponent keyName='name' name='ФИО' sorting={userState.adaptiveSorting['name']} changeHandle={(key)=>dispatch(setUserSorting(key))}/>
+                    <AdaptiveSortingComponent keyName='email' name='Почтовый адресс' sorting={userState.adaptiveSorting['email']} changeHandle={(key)=>dispatch(setUserSorting(key))}/>
+                </div>
             </div>
         </div>
     );
